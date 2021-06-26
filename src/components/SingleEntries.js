8cada3f0fa1798bea60723edd99cef3c5aaa381b
RadekKosiada <kosiadar@gmail.com>
2021-06-26 18:56:11 +0200

    Add click event to single entry

--- src/components/SingleEntries.js
+++ src/components/SingleEntries.js
@@ -2,13 +2,18 @@ import React from "react";
 
 export default function SingleEntries(props) {
   console.log(props.weatherEntriesArray);
+  function handleClick(event) {
+    console.log('id: ', event.target.id);
+  }
+  
   return (
     <div>
       {props.weatherEntriesArray.map((entry, index) => {
         return (
-          <div key={index} className="weather-entry">
+          <div key={index} id={index} className="weather-entry" onClick={handleClick}>
             <p>{entry.dt_txt}</p>
             <p>{entry.weather[0].icon}</p>
+            <p>{entry.weather[0].main}</p>
             <p>{entry.weather[0].description}</p>
             <p>{entry.main.temp}</p>
           </div>
