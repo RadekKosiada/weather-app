270f2eb900bf74f66e341ae4bc7abeff82866f8d
RadekKosiada <kosiadar@gmail.com>
2021-06-26 18:40:15 +0200

    Display all entries & add basic CSS

--- src/components/SingleEntries.js
+++ src/components/SingleEntries.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function SingleEntries(props) {
   console.log(props.weatherEntriesArray);
   return (
     <div>
-      <p>Single Entry</p>
+      {props.weatherEntriesArray.map((entry, index) => {
+        return (
+          <div key={index} className="weather-entry">
+            <p>{entry.dt_txt}</p>
+            <p>{entry.weather[0].icon}</p>
+            <p>{entry.weather[0].description}</p>
+            <p>{entry.main.temp}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
